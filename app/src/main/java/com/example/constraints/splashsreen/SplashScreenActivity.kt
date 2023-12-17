b95e79517e6b4a2b9ed95560d4f06d281b7c2d8c
wira07 <wiralodrasaputra07@gmail.com>
2023-12-18 00:56:49 +0700

    menambahkan animation

--- app/src/main/java/com/example/constraints/splashsreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/constraints/splashsreen/SplashScreenActivity.kt
@@ -14,10 +14,12 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, splashTimeOut)
+
     }
 }
